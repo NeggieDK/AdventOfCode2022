e2a90baeab4f43e0c6aea8a223cb32e7d2290127
Neggie <aarondk1@hotmail.com>
2022-12-15 01:00:21 +0100

    Day12 Part 1

--- AdventOfCode2022/Challenges/Day12.cs
+++ AdventOfCode2022/Challenges/Day12.cs
@@ -33,53 +33,68 @@ namespace AdventOfCode2022.Challenges
 
     public class Day12 : IDay
     {
+        private List<string> Lines;
+        private List<CurrentPosition> ViablePaths = new List<CurrentPosition>();
+        public int EndX { get; set; }
+        public int EndY { get; set; }
+
         private bool CheckLevel(char currentLevel, char nextLevel)
         {
-            if (currentLevel == nextLevel) return true;
+            if (nextLevel <= currentLevel && nextLevel != 'E') return true;
             if(currentLevel + 1 == nextLevel) return true;
             if ((currentLevel == 'z' || currentLevel == 'y') && nextLevel == 'E') return true;
 
             return false;
         }
 
-        private void CheckNeighbour(CurrentPosition result, int x, int y, List<string> lines, Queue<CurrentPosition> positionsToCheck, List<CurrentPosition> viablePaths)
+        private void CheckNeighbour(CurrentPosition result, int x, int y, PriorityQueue<CurrentPosition, int> positionsToCheck, HashSet<Tuple<int,int>> positionsAlreadyChecked)
         {
             var newResult = result.Clone();
-            newResult.CurrentLevel = lines[result.Y + y][result.X + x];
+            newResult.CurrentLevel = Lines[result.Y + y][result.X + x];
             newResult.X += x;
             newResult.Y += y;
             newResult.PositionsVisited.Add(new Tuple<int, int>(result.X, result.Y));
-            
 
-            if (lines[result.Y + y][result.X + x] == 'E')
+            if (Lines[result.Y + y][result.X + x] == 'E')
             {
-                viablePaths.Add(newResult);
+                ViablePaths.Add(newResult);
                 return;
             }
-            
-            positionsToCheck.Enqueue(newResult);
+
+            if (positionsAlreadyChecked.Contains(new Tuple<int, int>(newResult.X, newResult.Y))) return;
+
+            var manhattanDistance = Math.Abs(newResult.X - EndX) + Math.Abs(newResult.Y - EndY);
+            positionsToCheck.Enqueue(newResult, manhattanDistance + newResult.PositionsVisited.Count);
+            positionsAlreadyChecked.Add(new Tuple<int, int>(result.X, result.Y));
         }
 
         public void Part1()
         {
-            var lines = File.ReadAllLines("AoC/Input/Day12Part1.txt").ToList();
+            Lines = File.ReadAllLines("AoC/Input/Day12Part1.txt").ToList();
             var startingX = 0;
             var startingY = 0;
             var currentLevel = 'a';
 
-            var maxX = lines[0].Length - 1;
-            var maxY = lines.Count - 1;
+            var maxX = Lines[0].Length - 1;
+            var maxY = Lines.Count - 1;
 
-            for (int i = 0; i < lines.Count; i++)
+            for (int i = 0; i < Lines.Count; i++)
             {
-                string line = lines[i];
+                string line = Lines[i];
                 for (int i1 = 0; i1 < line.Length; i1++)
                 {
                     char element = line[i1];
-                    if (element != 'S') continue;
+                    if (element == 'S')
+                    {
+                        startingX = i1;
+                        startingY = i;
+                    }
+                    else if(element == 'E')
+                    {
+                        EndX = i1;
+                        EndY = i;
+                    }
 
-                    startingX = i1;
-                    startingY = i;
                 }
             }
 
@@ -89,45 +104,45 @@ namespace AdventOfCode2022.Challenges
                 X = startingX,
                 Y = startingY,
             };
-            var positionsToCheck = new Queue<CurrentPosition>();
-            positionsToCheck.Enqueue(startPosition);
+            var positionsToCheck = new PriorityQueue<CurrentPosition, int>();
+            positionsToCheck.Enqueue(startPosition, 0);
    
-            var viablePaths = new List<CurrentPosition>();
+            ViablePaths = new List<CurrentPosition>();
             var positionsAlreadyChecked = new HashSet<Tuple<int, int>>();
-            while(positionsToCheck.TryDequeue(out var result))
+            while(positionsToCheck.TryDequeue(out var result, out var _))
             {
                 if(result.X != 0)
                 {
-                    if(!result.PositionsVisited.Contains(new Tuple<int, int>(result.X - 1, result.Y)) && CheckLevel(result.CurrentLevel, lines[result.Y][result.X - 1]))
+                    if(!result.PositionsVisited.Contains(new Tuple<int, int>(result.X - 1, result.Y)) && CheckLevel(result.CurrentLevel, Lines[result.Y][result.X - 1]))
                     {
-                        CheckNeighbour(result, -1, 0, lines, positionsToCheck, viablePaths);
+                        CheckNeighbour(result, -1, 0, positionsToCheck, positionsAlreadyChecked);
                     }
                 }
                 if (result.X != maxX)
                 {
-                    if (!result.PositionsVisited.Contains(new Tuple<int, int>(result.X + 1, result.Y)) && CheckLevel(result.CurrentLevel, lines[result.Y][result.X + 1]))
+                    if (!result.PositionsVisited.Contains(new Tuple<int, int>(result.X + 1, result.Y)) && CheckLevel(result.CurrentLevel, Lines[result.Y][result.X + 1]))
                     {
-                        CheckNeighbour(result, 1, 0, lines, positionsToCheck, viablePaths);
+                        CheckNeighbour(result, 1, 0, positionsToCheck, positionsAlreadyChecked);
                     }
                 }
 
                 if (result.Y != 0)
                 {
-                    if (!result.PositionsVisited.Contains(new Tuple<int, int>(result.X, result.Y - 1)) && CheckLevel(result.CurrentLevel, lines[result.Y - 1][result.X]))
+                    if (!result.PositionsVisited.Contains(new Tuple<int, int>(result.X, result.Y - 1)) && CheckLevel(result.CurrentLevel, Lines[result.Y - 1][result.X]))
                     {
-                        CheckNeighbour(result, 0, -1, lines, positionsToCheck, viablePaths);
+                        CheckNeighbour(result, 0, -1, positionsToCheck, positionsAlreadyChecked);
                     }
                 }
                 if (result.Y != maxY)
                 {
-                    if (!result.PositionsVisited.Contains(new Tuple<int, int>(result.X, result.Y + 1)) && CheckLevel(result.CurrentLevel, lines[result.Y + 1][result.X]))
+                    if (!result.PositionsVisited.Contains(new Tuple<int, int>(result.X, result.Y + 1)) && CheckLevel(result.CurrentLevel, Lines[result.Y + 1][result.X]))
                     {
-                        CheckNeighbour(result, 0, 1, lines, positionsToCheck, viablePaths);
+                        CheckNeighbour(result, 0, 1, positionsToCheck, positionsAlreadyChecked);
                     }
                 }
             }
 
-            Console.WriteLine(viablePaths.Min(i => i.PositionsVisited.Count));
+            Console.WriteLine(ViablePaths.Min(i => i.PositionsVisited.Count));
         }
 
         public void Part2()
